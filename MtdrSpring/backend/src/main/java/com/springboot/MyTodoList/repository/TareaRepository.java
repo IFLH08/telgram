55396aed4961e2a930b497355401cadc7870ac27
IFLH08 <a01785078@tec.mx>
2026-04-15 15:00:48 -0600

    a

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/TareaRepository.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/TareaRepository.java
@@ -12,15 +12,15 @@ import org.springframework.data.jpa.repository.Query;
 public interface TareaRepository extends JpaRepository<Tarea, Long> {
     List<Tarea> findByUsuarioAsignadoIdUsuario(Long idUsuario);
 
-    // Punto 5: Mostrar la tabla de tasks del SPRINT actual haciendo JOIN con usuario
+    // Punto 5: Mostrar la tabla de tasks del SPRINT actual haciendo JOIN con
+    // usuario
     @Query(value = "SELECT t.ID_TAREA as idTarea, t.NOMBRE as nombreTarea, " +
-                   "t.DESCRIPCION as descripcion, e.NOMBRE_ESTADO as estado, " +
-                   "u.NOMBRE as usuarioAsignado, s.NOMBRE as nombreSprint " +
-                   "FROM TAREAS t " +
-                   "LEFT JOIN USUARIOS u ON t.ID_USUARIO_ASIGNADO = u.ID_USUARIO " +
-                   "LEFT JOIN ESTADOS_TAREA e ON t.ID_ESTADO = e.ID_ESTADO " +
-                   "JOIN SPRINTS s ON t.ID_SPRINT = s.ID_SPRINT " +
-                   "WHERE t.ID_SPRINT = (SELECT ID_SPRINT FROM SPRINTS ORDER BY FECHA_INICIO DESC FETCH FIRST 1 ROWS ONLY)",
-           nativeQuery = true)
+            "t.DESCRIPCION as descripcion, e.NOMBRE_ESTADO as estado, " +
+            "u.NOMBRE as usuarioAsignado, s.NOMBRE as nombreSprint " +
+            "FROM TAREAS t " +
+            "JOIN USUARIOS u ON t.ID_USUARIO_ASIGNADO = u.ID_USUARIO " +
+            "JOIN ESTADOS_TAREA e ON t.ID_ESTADO = e.ID_ESTADO " +
+            "JOIN SPRINTS s ON t.ID_SPRINT = s.ID_SPRINT " +
+            "WHERE t.ID_SPRINT = (SELECT ID_SPRINT FROM SPRINTS ORDER BY FECHA_INICIO DESC FETCH FIRST 1 ROWS ONLY)", nativeQuery = true)
     List<TareaSprintDTO> findTareasSprintActual();
 }
